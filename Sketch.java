a227bf687e5d01c698985ee5ad75e835b7258c23
ivannnc <junxian.chen24@ycdsbk12.ca>
2023-11-04 19:42:32 -0400

    .

--- Sketch.java
+++ Sketch.java
@@ -24,14 +24,8 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    /*
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-*/
+  
     draw_section_outlines();
     draw_section1();
     draw_section2();
@@ -71,28 +65,65 @@ public class Sketch extends PApplet {
    * draws the bottom left section
    */
   public void draw_section1(){
-    int intX = 0;
+   int intX = 0;
     int intY = 0;
 
-    for(int intRow = 0; intRow < 30; intRow++){
-      for(int intColumn = 0; intColumn < 30; intColumn++){
-        intX = 3 + 0;  //Instead of zero, calculate the proper intX location using 'intRow'
-        intY = 300 + 3 + 0; //Instead of zero, calculate the proper intY location using 'intColumn'
-
-        fill(255);
-        noStroke();
-        rect(intX, intY, 5, 5);
-
+    for(int intRow = 0; intRow < 60; intRow++) {
+      strokeWeight(5);
+      intY = 300 + (5 * intRow);
+      if(intRow % 4 == 0){
+        stroke(255, 255, 255);
       }
+      else if (intRow % 2 == 0){
+        stroke(255, 255, 255);
+      }
+      else if (intRow % 2 == 1){
+        stroke(45, 150, 207);
+      }
+      line(0, intY, 300, intY);
+    }
+
+    for(int intColumn = 0; intColumn < 30; intColumn++){
+      stroke(45, 150, 207);
+      intX = 600 + (10 * intColumn);
+      line(intX, 300, intX, 600);
+    }
+    
+    for(int intBlueRows = 0; intBlueRows < 60; intBlueRows++){
+      stroke(45, 150, 207);
+      intY = 300 * (5 + intBlueRows);
+      line(0, intY, 300, intY);
     }
   }
-
+    
   /**
    * Use the modulus operator and an if statement to select the color
    * Don't loop from 30 to 60 to shift everything over, just add 300 to x.
    */
   public void draw_section2(){
+    int intX = 0;
+    int intY = 0;
 
+    for (int intColumn = 0; intColumn < 60; intColumn++){
+      strokeWeight(5);
+      intX = 300 + (5 * intColumn);
+      if(intColumn % 4 == 0){
+        stroke(0, 0, 0);
+      }
+      else if (intColumn % 2 == 0){
+        stroke(255, 255, 255);
+      }
+      else if( intColumn % 2 == 1){
+        stroke(45, 150, 207);
+      }
+      line(intX, 300, intX, 600);
+    }
+    for(int intRow = 0; intRow < 30; intRow++){
+      stroke(45, 150, 207);
+      intY = 300 + (10 * intRow);
+      line(300, intY, 600, intY);
+
+    }
   }
 
   /**
@@ -100,24 +131,105 @@ public class Sketch extends PApplet {
    * Don't use multiple 'if' statements.
    */
   public void draw_section3(){
+    int intX = 0;
+    int intY = 0;
 
+    for(int intRow = 0; intRow < 60; intRow++) {
+      strokeWeight(5);
+      intY = 300 + (5 * intRow);
+      if(intRow % 4 == 0){
+        stroke(0, 0, 0);
+      }
+      else if (intRow % 2 == 0){
+        stroke(255, 255, 255);
+      }
+      else if (intRow % 2 == 1){
+        stroke(45, 150, 207);
+      }
+      line(600, intY, 900, intY);
+    }
+
+    for(int intColumn = 0; intColumn < 30; intColumn++){
+      stroke(45, 150, 207);
+      intX = 600 + (10 * intColumn);
+      line(intX, 300, intX, 600);
+    }
   }
 
   /**
    * Use the modulus operator and just one 'if' statement to select the color.
    */
   public void draw_section4(){
+    int intX = 0;
+    int intY = 0;
 
+    for(int intRow = 0; intRow < 60; intRow++){
+      for(int intColumn = 0; intColumn < 60; intColumn ++){
+        strokeWeight(5);
+          intY = 300 + (5 * intRow);
+          intX = 900 + (5 * intColumn);
+
+          // print out horizontal lines
+          if(intRow % 4 == 0){
+            stroke(0, 0, 0);
+          }
+          else if (intRow % 2 == 0){
+            noStroke();
+          }
+          else if(intRow % 2 == 1){
+            stroke(45, 150, 207);
+          }
+          line(900, intY, 1200, intY);
+
+          // print out vertical lines
+          if (intColumn % 4 == 0){
+            stroke(0, 0, 0);
+          }
+          else if(intRow % 2 == 0){
+            noStroke();
+          }
+          else if(intColumn % 2 == 1){
+            stroke(45, 150, 207);
+          }
+          line(intX, 300, intX, 600);
+      }
+    }
+
+    for(int intRow = 0; intRow < 30; intRow++){
+      for(int intColumn = 0; intColumn < 30; intColumn++){
+        intX = 905 + 10 * intColumn;
+        intY = 305 + 10 * intRow;
+
+        stroke(45, 150, 207);
+        line(intX, 300, intX, 600);
+        line(900, intY, 1200, intY);
+      }
+    }
   }
 
   /**
    * Do NOT use 'if' statements to complete 5-8. Manipulate the loops instead
    */
   public void draw_section5(){
+    int intX = 0;
+    int intY = 0;
 
+    for(int intRow = 0; intRow < 30; intRow++){
+      for(int intColumn = 0; intColumn < 30; intColumn++){
+        intX = 10 * intRow;
+        intY = 10 * intColumn;
+
+        stroke(45, 150, 207);
+        strokeWeight(5);
+        line(intX, 0, intX, 300);
+        line(0, intY, 300, intY);
+        line(intX, 0, 0, intY);
+      }
+    }
   }
 
   public void draw_section6(){
+    
 
   }
 
@@ -126,12 +238,21 @@ public class Sketch extends PApplet {
   }
   
   public void draw_section8(){
+  int intX = 0;
+  int intY = 0;
 
+  for(int intRow = 0; intRow < 30; intRow++){
+    for(int intColumn = 0; intColumn < 30; intColumn++) {
+      intX = 900 + 10 * intRow;
+      intY = 10 * intColumn;
+
+      stroke(45, 150, 207);
+      strokeWeight(5);
+      line(intX, 0, intX, 300);
+      line(900, intY, 1200, intY);
+      line(intX, 300, 900, intY);
+    }
   }
-
-
-
-
-
+  }
 
 }
